b12dac64ef1bc4747d84322787ce8fe55eae4e5f
marwa sahraoui <marwasahrawi1988@gmail.com>
2020-03-06 11:09:41 +0100

    commentaires supprimés

--- exemple0/src/exemple0/matériels.java
+++ exemple0/src/exemple0/matériels.java
@@ -3,10 +3,10 @@ package exemple0;
 public class matériels {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
         System.out.println("nchriw sel3a w neklo mekla");
         
-        //modifier le code et faire une bonne recette 
+        
 	}
 
 }
