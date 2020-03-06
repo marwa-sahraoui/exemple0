f83b77b0ee7626b2bad20cc644004ec07301d5c4
marwa sahraoui <marwasahrawi1988@gmail.com>
2020-03-06 11:07:18 +0100

    code actuel in master

---
+++ exemple0/src/exemple0/matériels.java
@@ -0,0 +1,12 @@
+package exemple0;
+
+public class matériels {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("nchriw sel3a w neklo mekla");
+        
+        //modifier le code et faire une bonne recette 
+	}
+
+}
